de4c0b1ffa3d54fad873e2ee853668fcc08aa9ae
Guilherme Davi Gomes de Lima <41553712+Guilhermedgl@users.noreply.github.com>
2019-04-21 10:08:37 -0300

    Update movies.js

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -113,114 +113,114 @@ function turnHoursToMinutes(moviesArray) {
 }
 console.log(turnHoursToMinutes(movies));
 
-// Get the average of all rates with 2 decimals
-// function ratesAverage(moviesArray) {
-//   var average = 0;
+Get the average of all rates with 2 decimals
+function ratesAverage(moviesArray) {
+  var average = 0;
 
-//   moviesArray.forEach(function (e) {
-//     if (e.rate !== '') {
-//       average += parseFloat(e.rate);
-//     } else {
-//       average += 0;
-//     }
-//   });
+  moviesArray.forEach(function (e) {
+    if (e.rate !== '') {
+      average += parseFloat(e.rate);
+    } else {
+      average += 0;
+    }
+  });
 
-//   average = (average / moviesArray.length).toFixed(2);
-//   return parseFloat(average);
-// }
+  average = (average / moviesArray.length).toFixed(2);
+  return parseFloat(average);
+}
 
-// // Get the average of Drama Movies
-// function dramaMoviesRate(moviesArray) {
-//   var dramaMovies = moviesArray.filter(enterObject);
+// Get the average of Drama Movies
+function dramaMoviesRate(moviesArray) {
+  var dramaMovies = moviesArray.filter(enterObject);
 
-//   function enterObject(e) {
-//     var g = e.genre;
+  function enterObject(e) {
+    var g = e.genre;
 
-//     for (var i = 0; i < g.length; i++) {
-//       if (g[i] === 'Drama') {
-//         return true;
-//       }
-//     }
-//     return false;
-//   }
+    for (var i = 0; i < g.length; i++) {
+      if (g[i] === 'Drama') {
+        return true;
+      }
+    }
+    return false;
+  }
 
-//   if (dramaMovies.length === 0) {
-//     return undefined;
-//   }
-//   return ratesAverage(dramaMovies);
-// }
+  if (dramaMovies.length === 0) {
+    return undefined;
+  }
+  return ratesAverage(dramaMovies);
+}
 
-// // Order by time duration, in growing order
-// function orderByDuration(moviesArray) {
-//   durationArray = turnHoursToMinutes(moviesArray);
-//   durationArray.sort(compareDuration);
+// Order by time duration, in growing order
+function orderByDuration(moviesArray) {
+  durationArray = turnHoursToMinutes(moviesArray);
+  durationArray.sort(compareDuration);
 
-//   function compareDuration(obj1, obj2) {
-//     if (obj1.duration === obj2.duration) {
-//       if (obj1.title < obj2.title) {
-//         return -1;
-//       } if (obj1.title > obj2.title) {
-//         return 1;
-//       }
-//       return 0;
-//     }
-//     return obj1.duration - obj2.duration;
-//   }
+  function compareDuration(obj1, obj2) {
+    if (obj1.duration === obj2.duration) {
+      if (obj1.title < obj2.title) {
+        return -1;
+      } if (obj1.title > obj2.title) {
+        return 1;
+      }
+      return 0;
+    }
+    return obj1.duration - obj2.duration;
+  }
 
-//   return durationArray;
-// }
+  return durationArray;
+}
 
-// // How many movies did STEVEN SPIELBERG
-// function howManyMovies(moviesArray) {
-//   if (moviesArray.length === 0) {
-//     return undefined;
-//   }
-//   var dramaMovies = moviesArray.filter(enterObject);
+// How many movies did STEVEN SPIELBERG
+function howManyMovies(moviesArray) {
+  if (moviesArray.length === 0) {
+    return undefined;
+  }
+  var dramaMovies = moviesArray.filter(enterObject);
 
-//   function enterObject(e) {
-//     var g = e.genre;
-//     var dir = e.director;
+  function enterObject(e) {
+    var g = e.genre;
+    var dir = e.director;
 
-//     if (dir === 'Steven Spielberg') {
-//       for (var i = 0; i < g.length; i++) {
-//         if (g[i] === 'Drama') {
-//           return true;
-//         }
-//       }
-//       return false;
-//     }
-//   }
+    if (dir === 'Steven Spielberg') {
+      for (var i = 0; i < g.length; i++) {
+        if (g[i] === 'Drama') {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
 
-//   var stevenPhrase = 'Steven Spielberg directed ';
+  var stevenPhrase = 'Steven Spielberg directed ';
 
-//   if (dramaMovies.length !== 0) {
-//     return stevenPhrase + String(dramaMovies.length) + ' drama movies!';
-//   }
-//   return stevenPhrase + 0 + ' drama movies!';
-// }
+  if (dramaMovies.length !== 0) {
+    return stevenPhrase + String(dramaMovies.length) + ' drama movies!';
+  }
+  return stevenPhrase + 0 + ' drama movies!';
+}
 
-// // Order by title and print the first 20 titles
-// function orderAlphabetically(moviesArray) {
-//   moviesArray.sort(compareDuration);
+// Order by title and print the first 20 titles
+function orderAlphabetically(moviesArray) {
+  moviesArray.sort(compareDuration);
 
-//   function compareDuration(obj1, obj2) {
-//     if (obj1.title < obj2.title) {
-//       return -1;
-//     } if (obj1.title > obj2.title) {
-//       return 1;
-//     }
-//     return 0;
-//   }
-//   var namesArray = [];
-//   moviesArray.forEach(function (e) {
-//     namesArray.push(e.title);
-//   });
+  function compareDuration(obj1, obj2) {
+    if (obj1.title < obj2.title) {
+      return -1;
+    } if (obj1.title > obj2.title) {
+      return 1;
+    }
+    return 0;
+  }
+  var namesArray = [];
+  moviesArray.forEach(function (e) {
+    namesArray.push(e.title);
+  });
 
-//   if (namesArray.length >= 20) {
-//     return smallArray = namesArray.slice(0, 20);
-//   }
+  if (namesArray.length >= 20) {
+    return smallArray = namesArray.slice(0, 20);
+  }
 
-//   return namesArray;
-// }
+  return namesArray;
+}
 
 // Best yearly rate average
